912e49d2680f8e465730492244e367489fa7c4ac
Ryan Tracey <ryan.m.tracey@gmail.com>
2016-04-02 14:59:41 -0400

    add redux and hook it up to app

--- views/RootView/index.js
+++ views/RootView/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-//import ReactDOM from 'react-dom'
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider, connect } from 'react-redux'
 
@@ -11,10 +11,65 @@ export default class RootView extends React.Component {
 
   render () {
     return (
-      <div>
-        <h3>Welcome To The Exercise</h3>
-        {this.props.children}
-      </div>
+      <Provider store={store}>
+        <App />
+      </Provider>
     );
   }
 }
+
+class Counter extends Component {
+  render() {
+    const { value, onIncreaseClick } = this.props
+    return (
+      <div>
+        <h3>Welcome To The Exercise</h3>
+        {this.props.children}
+        <span>{value}</span>
+        <button onClick={onIncreaseClick}>Increase</button>
+      </div>
+    )
+  }
+}
+
+Counter.propTypes = {
+  value: PropTypes.number.isRequired,
+  onIncreaseClick: PropTypes.func.isRequired
+}
+
+// Action
+const increaseAction = { type: 'increase' }
+
+// Reducer
+function counter(state = { count: 0 }, action) {
+  let count = state.count
+  switch (action.type) {
+    case 'increase':
+      return { count: count + 1 }
+    default:
+      return state
+  }
+}
+
+// Store
+let store = createStore(counter)
+
+// Map Redux state to component props
+function mapStateToProps(state) {
+  return {
+    value: state.count
+  }
+}
+
+// Map Redux actions to component props
+function mapDispatchToProps(dispatch) {
+  return {
+    onIncreaseClick: () => dispatch(increaseAction)
+  }
+}
+
+// Connected Component
+let App = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter)
