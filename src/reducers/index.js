45e0b3c605d5561f0490949ad0ffcdf0962edfef
Ryan Tracey <ryan.m.tracey@gmail.com>
2016-04-03 14:08:17 -0400

    good progress on autocomplete

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,11 @@
-import { REQUEST_USERS, RECEIVE_USERS, REQUEST_PROFILE, RECEIVE_PROFILE } from '../actions'
+import {
+  REQUEST_USERS,
+  RECEIVE_USERS,
+  REQUEST_PROFILE,
+  RECEIVE_PROFILE,
+  REQUEST_SEARCH,
+  RECEIVE_SEARCH
+ } from '../actions'
 
 let counter = function counter(state = { count: 0, users: [] }, action) {
   let count = state.count
@@ -10,11 +17,35 @@ let counter = function counter(state = { count: 0, users: [] }, action) {
     case REQUEST_USERS:
       return { loading: true, count: count };
     case RECEIVE_USERS:
-      return { loading: false, count: count, users: action.users };
+      return {
+        loading: false,
+        count: count,
+        users: action.users
+      };
     case REQUEST_PROFILE:
-      return { loading: true, count: count, users: users };
+      return { loading: true,
+        count: count,
+        users: users
+      };
     case RECEIVE_PROFILE:
-      return { loading: false, count: count, profile: action.profile, users: users };
+      return {
+        loading: false,
+        count: count,
+        profile: action.profile,
+        users: users
+      };
+    case REQUEST_SEARCH:
+      return { loading: true,
+        count: count,
+        users: users,
+        query: action.query
+      };
+    case RECEIVE_SEARCH:
+      return { loading: false,
+        count: count,
+        users: action.users,
+        query: query
+      };
     default:
       return state;
   }
